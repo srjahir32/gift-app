a9da88deab97fe9a2fc63cb30fa6d14030fbb378
srjahir32 <srjahir32@gmail.com>
2016-10-15 13:41:46 +0530

    add express

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
 var express = require('express');
 var app = express();
-
+var router = express.Router();
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,token');
+    //res.setHeader('*');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+router.get('/',function(req,res){
+	  //res.header("Access-Control-Allow-Origin", "*");
+	  res.sendFile("index.html", {"root": 'views'});
+});
+
+app.get('/', function(req, res) {
+       	// load the single view file (angular will handle the page changes on the front-end)
+    	res.sendFile("index.html", {"root": 'views'});
+    });
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set('view engine', 'js');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
 // set the home page route
-app.get('/', function(req, res) {
+ app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
 	res.render('index');
